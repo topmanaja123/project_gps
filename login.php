030347b1a1c8917b4d20b029bc218f70305b4a34
BaBaRa <fanta.orange_@hotmail.com>
2019-09-03 14:21:37 +0700

    test page index

--- login.php
+++ login.php
@@ -7,45 +7,46 @@
   <meta http-equiv="X-UA-Compatible" content="ie=edge">
   <title>Document</title>
   <link rel="stylesheet" href="css/bootstrap.min.css">
-  <link rel="stylesheet" href="css/login.css">
+  <link rel="stylesheet" href="css/login.scss">
   <link rel="stylesheet" href="fontawesome/all.css">
   <link href='https://fonts.googleapis.com/css?family=Montserrat' rel='stylesheet' type='text/css'>
 
 </head>
 
-<body>
-  <div id="particles-js"></div>
+<body class="align">
+<div id="particles-js"></div>
   <!-- /.particles div -->
-  <div class="background-wrap">
-  <div class="background"></div>
-</div>
+  </body>
+	
 
-<form id="accesspanel" action="" method="post">
-  <h1 id="litheader">AECEND</h1>
-  <div class="inset">
-    <p>
-      <input type="text" name="username" id="email" placeholder="Email address">
-    </p>
-    <p>
-      <input type="password" name="password" id="password" placeholder="Access code">
-    </p>
-    <div style="text-align: center;">
-      <div class="checkboxouter">
-        <input type="checkbox" name="rememberme" id="remember" value="Remember">
-        <label class="checkbox"></label>
-      </div>
-      <label for="remember">Remember me for 14 days</label>
-    </div>
-    <input class="loginLoginValue" type="hidden" name="service" value="login" />
+  <div class="grid">
+
+    <div id="login">
+
+      <h2><span class="fontawesome-lock"></span>Sign In</h2>
+      <form action="#" method="POST">
+        <fieldset>
+          <p><label for="email">E-mail address</label></p>
+          <p><input type="email" id="email" placeholder="mail@address.com"></p>
+
+          <p><label for="password">Password</label></p>
+          <p><input type="password" id="password" placeholder="password"></p>
+
+          <p><input type="submit" value="Sign In"></p>
+
+        </fieldset>
+
+      </form>
+
+    </div> <!-- end login -->
+
   </div>
-  <p class="p-container">
-    <input type="submit" name="Login" id="go" value="Authorize">
-  </p>
-</form>
-</body>
+
+</body>	
+
 
 </html>
 
-<!-- <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script> -->
-<script src="vendor\login\particles.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
+<script src="js\particles.js"></script>
 <script src="js/login.js"></script>
