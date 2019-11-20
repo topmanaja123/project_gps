59f97fbfb08bae7ce05aecf7d0a29af6530dfb8b
topmanaja123 <top3456@hotmail.com>
2019-11-20 17:06:55 +0700

    change limit car

--- getPositionsT.php
+++ getPositionsT.php
@@ -103,7 +103,7 @@ INNER JOIN `positions` ON `devices`.`positionid` = `positions`.`id` ";
 if ($sc) {
   $strSQL .= " WHERE name LIKE '%$sc%' ";
 }
-$strSQL .= " LIMIT 20";
+$strSQL .= " LIMIT 200";
 $objQuery = $conn->query($strSQL) or die(mysql_error());
 $intNumField = mysqli_num_fields($objQuery);
 $resultArray = array();
